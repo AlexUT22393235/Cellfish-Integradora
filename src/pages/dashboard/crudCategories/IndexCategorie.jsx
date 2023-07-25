f854a169ff8ed8286f4c0aa518b856f120c3656a
AlexUT22393235 <22393235@utcancun.edu.mx>
2023-07-25 02:47:50 -0500

    Segundo dashboard

--- src/pages/dashboard/crudCategories/IndexCategorie.jsx
+++ src/pages/dashboard/crudCategories/IndexCategorie.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import DashboardSidebar from "../dashboardSideBar";
 
+
 export default function IndexCategorie (){
     return(
         <>
@@ -9,18 +10,22 @@ export default function IndexCategorie (){
     <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
         <thead class="text-xs text-gray-700 uppercase dark:text-gray-400">
             <tr>
-                <th scope="col" class="px-6 py-3 bg-gray-50 dark:bg-gray-800">
-                    Product name
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    Color
+            <th scope="col" class="px-6 py-3 bg-gray-50 dark:bg-gray-800">
+                    #
                 </th>
                 <th scope="col" class="px-6 py-3 bg-gray-50 dark:bg-gray-800">
-                    Category
+                    Nombre
                 </th>
                 <th scope="col" class="px-6 py-3">
-                    Price
+                    Imagen
                 </th>
+                <th scope="col" class="px-6 py-3">
+                    Actualizar
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Eliminar
+                </th>
+            
             </tr>
         </thead>
         <tbody>
@@ -31,69 +36,9 @@ export default function IndexCategorie (){
                 <td class="px-6 py-4">
                     Silver
                 </td>
-                <td class="px-6 py-4 bg-gray-50 dark:bg-gray-800">
-                    Laptop
-                </td>
-                <td class="px-6 py-4">
-                    $2999
-                </td>
+            
             </tr>
-            <tr class="border-b border-gray-200 dark:border-gray-700">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
-                    Microsoft Surface Pro
-                </th>
-                <td class="px-6 py-4">
-                    White
-                </td>
-                <td class="px-6 py-4 bg-gray-50 dark:bg-gray-800">
-                    Laptop PC
-                </td>
-                <td class="px-6 py-4">
-                    $1999
-                </td>
-            </tr>
-            <tr class="border-b border-gray-200 dark:border-gray-700">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
-                    Magic Mouse 2
-                </th>
-                <td class="px-6 py-4">
-                    Black
-                </td>
-                <td class="px-6 py-4 bg-gray-50 dark:bg-gray-800">
-                    Accessories
-                </td>
-                <td class="px-6 py-4">
-                    $99
-                </td>
-            </tr>
-            <tr class="border-b border-gray-200 dark:border-gray-700">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
-                    Google Pixel Phone
-                </th>
-                <td class="px-6 py-4">
-                    Gray
-                </td>
-                <td class="px-6 py-4 bg-gray-50 dark:bg-gray-800">
-                    Phone
-                </td>
-                <td class="px-6 py-4">
-                    $799
-                </td>
-            </tr>
-            <tr>
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
-                    Apple Watch 5
-                </th>
-                <td class="px-6 py-4">
-                    Red
-                </td>
-                <td class="px-6 py-4 bg-gray-50 dark:bg-gray-800">
-                    Wearables
-                </td>
-                <td class="px-6 py-4">
-                    $999
-                </td>
-            </tr>
+            
         </tbody>
     </table>
 </div>
--- src/pages/dashboard/crudProdcuts/IndexProducts.jsx
+++ src/pages/dashboard/crudProdcuts/IndexProducts.jsx
@@ -1,102 +1,121 @@
-import React from "react";
-import DashboardSidebar from "../dashboardSideBar";
 
-export default function IndexProducts (){
-    return(
+import DashboardSidebar from "../dashboardSideBar";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+
+
+export default function IndexProducts() {
+
+    const [productos, setProductos] = useState([]);
+
+
+
+//V.1
+
+    useEffect(() => {
+        // Realizar la solicitud GET a la API utilizando Axios
+        axios.get('http://localhost:3001/api/productos')
+            .then(response => setProductos(response.data))
+            .catch(error => console.error('Error al obtener los datos:', error));
+    }, []);
+
+    
+    const handleEliminarProducto = (id) => {
+        // Verificar la URL antes de hacer la solicitud DELETE
+        console.log(`URL para eliminar el producto con ID ${id}: http://localhost:3001/api/productos/${id}`);
+        // Realizar la solicitud DELETE a la API utilizando Axios
+        axios.delete(`http://localhost:3001/api/productos/${id}`)
+          .then(response => {
+            // Actualizar la lista de productos después de eliminar el producto
+            setProductos(prevProductos => prevProductos.filter(producto => producto.id !== id));
+          })
+          .catch(error => console.error('Error al eliminar el producto:', error));
+      };
+
+
+    return (
         <>
-        <DashboardSidebar></DashboardSidebar>
-<div class="relative overflow-x-auto shadow-md sm:rounded-lg p-4 sm:ml-64">
-    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-        <thead class="text-xs text-gray-700 uppercase dark:text-gray-400">
-            <tr>
-                <th scope="col" class="px-6 py-3 bg-gray-50 dark:bg-gray-800">
-                    Product name
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    Color
-                </th>
-                <th scope="col" class="px-6 py-3 bg-gray-50 dark:bg-gray-800">
-                    Category
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    Price
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr class="border-b border-gray-200 dark:border-gray-700">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
-                    Apple MacBook Pro 17"
-                </th>
-                <td class="px-6 py-4">
-                    Silver
-                </td>
-                <td class="px-6 py-4 bg-gray-50 dark:bg-gray-800">
-                    Laptop
-                </td>
-                <td class="px-6 py-4">
-                    $2999
-                </td>
-            </tr>
-            <tr class="border-b border-gray-200 dark:border-gray-700">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
-                    Microsoft Surface Pro
-                </th>
-                <td class="px-6 py-4">
-                    White
-                </td>
-                <td class="px-6 py-4 bg-gray-50 dark:bg-gray-800">
-                    Laptop PC
-                </td>
-                <td class="px-6 py-4">
-                    $1999
-                </td>
-            </tr>
-            <tr class="border-b border-gray-200 dark:border-gray-700">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
-                    Magic Mouse 2
-                </th>
-                <td class="px-6 py-4">
-                    Black
-                </td>
-                <td class="px-6 py-4 bg-gray-50 dark:bg-gray-800">
-                    Accessories
-                </td>
-                <td class="px-6 py-4">
-                    $99
-                </td>
-            </tr>
-            <tr class="border-b border-gray-200 dark:border-gray-700">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
-                    Google Pixel Phone
-                </th>
-                <td class="px-6 py-4">
-                    Gray
-                </td>
-                <td class="px-6 py-4 bg-gray-50 dark:bg-gray-800">
-                    Phone
-                </td>
-                <td class="px-6 py-4">
-                    $799
-                </td>
-            </tr>
-            <tr>
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
-                    Apple Watch 5
-                </th>
-                <td class="px-6 py-4">
-                    Red
-                </td>
-                <td class="px-6 py-4 bg-gray-50 dark:bg-gray-800">
-                    Wearables
-                </td>
-                <td class="px-6 py-4">
-                    $999
-                </td>
-            </tr>
-        </tbody>
-    </table>
-</div>
+            <DashboardSidebar></DashboardSidebar>
+            <div className="relative overflow-x-auto shadow-md sm:rounded-lg p-4 sm:ml-64">
+                <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                    <thead className="text-xs text-gray-700 uppercase dark:text-gray-400">
+                        <tr>
+                            <th scope="col" className="px-6 py-3 bg-gray-50 dark:bg-gray-800">
+                                #
+                            </th>
+                            <th scope="col" className="px-6 py-3 bg-gray-50 dark:bg-gray-800">
+                                Nombre
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Descripcion
+                            </th>
+                            <th scope="col" className="px-6 py-3 bg-gray-50 dark:bg-gray-800">
+                                Precio
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Imagen
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Actualizar
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Eliminar
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    
+                        {productos.map((producto) => (
+                            console.log("ID del producto a eliminar:", producto.id_producto),
+                        
+                            <tr key={producto.id_producto} className="border-b border-gray-200 dark:border-gray-700">
+                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
+                                    {producto.id_producto}
+                                </th>
+                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
+                                    {producto.nombre}
+                                </th>
+                                <td className="px-6 py-4">
+                                    {producto.descripcion}
+                                </td>
+                                <td className="px-6 py-4 bg-gray-50 dark:bg-gray-800">
+                                    {producto.precio}
+                                </td>
+                                <td className="px-6 py-4">
+                                    {producto.imagen}
+                                </td>
+                                <td className="px-6 py-4">
+                                    {/* Botón para Actualizar */}
+                                    <button className="text-blue-600 dark:text-blue-400">
+                                        <i className='bx bx-pencil'><box-icon name='edit' type='solid' ></box-icon></i>
+                                    </button>
+                                </td>
+
+                                <td className="px-6 py-4">
+                                    {/* Botón para Eliminar */}
+                                    <button
+    className="text-red-600 dark:text-red-400"
+    onClick={() => {
+        console.log("ID del producto a eliminar:", producto.id_producto);
+        handleEliminarProducto(producto.id_producto);
+    }}
+>
+    <box-icon type='solid' name='trash-alt'></box-icon>
+</button>
+                                </td>
+
+                                {/* Renderizar más columnas según los datos de tu API */}
+                            </tr>
+                    
+                        ))}
+
+
+                        
+                    </tbody>
+                </table>
+                <button type="button" className="text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800 mt-5">Crear nuevo producto</button>
+
+            </div>
 
         </>
     )
